d445ffc70d21cc61ec01ea0717ad10ded8d2b610
moujianming <31647832+Moujianming@users.noreply.github.com>
2018-04-18 15:37:10 +0800

    test

---
+++ src/网易2018校园招聘编程题真题集合/Test4.java
@@ -0,0 +1,11 @@
+package 网易2018校园招聘编程题真题集合;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("moujianming");
+	}
+
+}
