f1ece4fdefc6c418736d94b8b3a8804a0a69cafa
moujianming <31647832+Moujianming@users.noreply.github.com>
2018-04-18 19:27:20 +0800

    非递归遍历二叉树,构造双向链表

--- src/剑指offer/Test25_二叉搜索树与双向链表.java
+++ src/剑指offer/Test25_二叉搜索树与双向链表.java
@@ -2,6 +2,7 @@ package 剑指offer;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Stack;
 
 /**
  * 
@@ -9,7 +10,7 @@ import java.util.ListIterator;
  *2018年4月8日:下午7:05:51
  * 问题描述:输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
-public class Test25_二叉搜索树 {
+public class Test25_二叉搜索树与双向链表  {
 	static LinkedList<TreeNode> list = new LinkedList<>();
 	public static void GetLink(TreeNode rootNode)
 	{
@@ -64,8 +65,44 @@ public class Test25_二叉搜索树 {
 	     return root;
 	    }
 
+	 //采用非递归的中序遍历进行
+	 public  TreeNode ConvertII(TreeNode pRootOfTree) {
+	       TreeNode p = pRootOfTree;
+	       TreeNode pre = null;
+	       TreeNode root = null;
+	       boolean isFirst = true;
+	       Stack<TreeNode> stack = new Stack<>();
+	       while(p!=null||!stack.empty())
+	       {
+	    	   if(p!=null)
+	    	   {
+	    		   stack.push(p);
+	    		   p = p.left;
+	    	   }
+	    	   else
+	    	   {
+	    		   p = stack.pop();
+	    		//System.out.println(p.val);
+	    		   if(isFirst)
+	    		   {
+	    			   root = p;
+	    			   pre = root;
+	    			   p = p.right;
+	    			   isFirst = false;
+	    		   }else
+	    		   {
+	    			   pre.right = p;
+	    			   p.left = pre;
+	    			   pre = p;
+	    			   p = p.right;
+	    		   }
+	    	   }
+	       }
+	        
+	        return root;
+	    }
 	public static void main(String[] args) {
-		/* TreeNode node1 = new TreeNode(12);
+		 TreeNode node1 = new TreeNode(12);
 		 TreeNode node2 = new TreeNode(5);
 		 TreeNode node3 = new TreeNode(18);
 		 TreeNode node4 = new TreeNode(2);
@@ -77,8 +114,8 @@ public class Test25_二叉搜索树 {
 		 node1.right = node3;
 		 node3.left = node5;
 		 node5.right = node6;
-		 node3.right = node7;*/
-		 TreeNode node1 = new TreeNode(5);
+		 node3.right = node7;
+		 /*TreeNode node1 = new TreeNode(5);
 		 TreeNode node2 = new TreeNode(4);
 		 TreeNode node3 = new TreeNode(3);
 		 TreeNode node4 = new TreeNode(2);
@@ -86,8 +123,14 @@ public class Test25_二叉搜索树 {
 		 node1.left = node2;
 		 node2.right = node3;
 		 node3.right = node4;
-		 node4.right = node5;
-		 new Test25_二叉搜索树().Convert(node1);
+		 node4.right = node5;*/
+		 new Test25_二叉搜索树与双向链表().ConvertII(node1);
+		 System.out.println(new String("moujianming").hashCode());
+		 System.out.println(new Integer(20000).hashCode());
+		 System.out.println(new String("moujianming").hashCode());
+		 System.out.println(new String("moujianming").hashCode());
+		 Character s = new Character('a');
+		 System.out.println(s.hashCode());
 	}
 }
 //用时1小时20分
